e0eb07e357f76cb4e24873c02b8b1959bdffd17f
PRAMODNAIK-DEV <pramodnaik.dev@gmail.com>
2025-10-20 12:34:33 +0530

    Completed Pydantic

--- Test/main.py
+++ Test/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 
-# Create a FastAPI app instance
 app = FastAPI()
 
-# Define a root route (homepage)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to your first FastAPI app!"}
